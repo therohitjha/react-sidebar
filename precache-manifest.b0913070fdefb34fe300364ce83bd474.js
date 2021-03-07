2ebd85db201b8dd0d156237495b5d7b1ff0b4005
Rohit Jha <therohitjha@gmail.com>
2021-03-07 23:51:12 +0530

    Updates

---
+++ precache-manifest.b0913070fdefb34fe300364ce83bd474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b5c332154ab65c491ec0280aae74a8",
+    "url": "/react-sidebar/index.html"
+  },
+  {
+    "revision": "a7f35730c64c20b3bb25",
+    "url": "/react-sidebar/static/css/main.ce3d3fbf.chunk.css"
+  },
+  {
+    "revision": "be211514af6390781bef",
+    "url": "/react-sidebar/static/js/2.e5cd5b44.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react-sidebar/static/js/2.e5cd5b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f35730c64c20b3bb25",
+    "url": "/react-sidebar/static/js/main.4df43eb6.chunk.js"
+  },
+  {
+    "revision": "b78b0b7f91a5ec14d0ed",
+    "url": "/react-sidebar/static/js/runtime-main.ac2297cf.js"
+  }
+]);
